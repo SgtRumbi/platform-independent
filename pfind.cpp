f64854b4ff00df4d6c6a28f54f52e435ffeb23e3
= <=>
2017-06-02 13:13:45 +0200

    Started Implementing a Linux GL Example

--- pfind.cpp
+++ pfind.cpp
@@ -13,12 +13,77 @@
 
 static void
 OpenGLLegacyFallback() {
-
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0f, 0.0f);
+    glVertex2f(1.0f, 0.0f);
+    glVertex2f(1.0f, 1.0f);
+    glEnd();
 }
 
 static void
 ModernOpenGL() {
+    static bool32 Initialized = false;
+    static uint32 ProgramHandle = -1;
+    if(!Initialized) {
+        Initialized = true;
+        
+        char *VertexShaderCode = "" 
+        "#version 150 core\n"
+        "attribute vec2 Position;"
+        
+        "void main(void) {"
+        "    gl_Position = vec4(Position, 0.0, 1.0);"
+        "}";
+        char *FragmentShaderCode = ""
+        "#version 150 core\n"
+        
+        "void main(void) {"
+        "    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+        "}";
+        
+        uint32 VertexShaderHandle = glCreateShader(GL_VERTEX_SHADER);
+        uint32 FragmentShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
+        
+        glShaderSource(VertexShaderHandle, VertexShaderCode);
+        glShaderSource(FragmentShaderHandle, FragmentShaderHandle);
+        
+        glCompileShader(VertexShaderHandle);
+        glCompileShader(FragmentShaderHandle);
+        
+        ProgramHandle = glCreateProgram();
+        glAttachShader(ProgramHandle, VertexShaderHandle);
+        glAttachShader(ProgramHandle, FragmentShaderHandle);
+        
+        glLinkProgram(ProgramHandle);
+    }
 
+    static int32 BufferHandle = 1;
+    
+    /* glEnable(GL_CALL);
+    glCullFace */
+    
+    /* if(!BufferHandle) {
+        glGenBuffers(1, &BufferHandle);
+    } */
+    
+    /* typedef float real32;
+    typedef double real64; */
+    
+    real32 Vertices[6] = {
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f
+    };
+    
+#if 1
+    glUseProgram(ProgramHandle);
+
+    glBindBuffer(GL_ARRAY_BUFFER, BufferHandle);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(real32),& Vertices[0], GL_STREAM_DRAW);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+#endif
 }
 
 static void
